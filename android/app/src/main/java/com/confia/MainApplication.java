29129e319c82727df311eaa7e284e966b865d35d
Guillermo <guillhermoherrera@gmail.com>
2020-05-26 19:32:59 -0500

    react-native-device-info
    los nuevos archivos dentro de ./android se crearon solos

--- android/app/src/main/java/com/confia/MainApplication.java
+++ android/app/src/main/java/com/confia/MainApplication.java
@@ -11,6 +11,11 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import java.util.Arrays;
+import com.facebook.react.shell.MainReactPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -22,11 +27,15 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
+          return Arrays.<ReactPackage>asList(
+              new MainReactPackage(),
+              new RNDeviceInfo(),
+              new RNGestureHandlerPackage()
+              //new RNGeocoderPackage(),
+              //new RNGestureHandlerPackage(),
+              //new VectorIconsPackage(),
+              //new RNBootSplashPackage()
+          );
         }
 
         @Override
