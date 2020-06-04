15bc8e76c68d3b9e1d559f1fa88292cb25308cda
Guillermo <guillhermoherrera@gmail.com>
2020-06-03 19:29:50 -0500

    ValidateCode, error, success

--- src/components/screens/Vale/index.js
+++ src/components/screens/Vale/index.js
@@ -3,5 +3,8 @@ import CustomerInformation from './CustomerInformation';
 import TransactionTypes from './TransactionType';
 import ValeSection from './ValeSection';
 import Reasons from './Reasons';
+import ValidateCode from './ValidateCode';
+import ErrorScreen from './Error';
+import SuccessCredit from './SuccessCredit';
 
-export {NewVale, CustomerInformation, TransactionTypes, ValeSection, Reasons};
+export {NewVale, CustomerInformation, TransactionTypes, ValeSection, Reasons, ValidateCode, ErrorScreen, SuccessCredit};
