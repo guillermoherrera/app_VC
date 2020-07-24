b633c176287346d2199daf19140dea1d9f6d61e1
Guillermo <guillhermoherrera@gmail.com>
2020-07-24 09:30:03 -0500

    accesoConfiaShop

--- src/components/screens/ConfiaShop/ConfiaShop.js
+++ src/components/screens/ConfiaShop/ConfiaShop.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { WebView } from 'react-native-webview'
 //import { WebView } from "react-native-webview-messaging/WebView";
-import { View, SafeAreaView, StatusBar, Platform, AsyncStorage } from 'react-native'
-import { Container } from 'native-base'
+import { View, SafeAreaView, StatusBar, Platform, AsyncStorage, Text } from 'react-native'
+import { Container, Content, Icon } from 'native-base'
 import { getProfile, setTicket, getTicket, removeTicket } from '../../../store/actions';
 import { Loading } from '../../common';
 import { colors } from '../../../assets';
+import { moderateScale } from 'react-native-size-matters';
 
 class ConfiaShop extends Component {  
   state = {
@@ -70,7 +71,7 @@ class ConfiaShop extends Component {
         <StatusBar animated barStyle={Platform.OS === 'android' ? 'light-content' : 'dark-content'} />
         <View style={{ flex: 1 }}>
           <SafeAreaView style={{ flex: 1, backgroundColor: colors.white }}>
-            <WebView
+            {profile.accesoConfiaShop ? <WebView
               ref={webview => { this.webview = webview; }}
               startInLoadingState
               javaScriptEnabledAndroid={true}
@@ -78,7 +79,10 @@ class ConfiaShop extends Component {
               javaScriptEnabled={true}
               onMessage={(event) => this._setTicket(event.nativeEvent.data)}
               source={{ uri: `https://confia-${this.state.env}.supernova-desarrollo.com/?meta=1&page=mobile&env=dist&tk1=${user.DistribuidorId}&tk2=&benefit=${user.categoriaId}` }}
-            />
+            /> : <View style={{alignItems: "center", flex: 1, paddingTop: moderateScale(100), paddingHorizontal: moderateScale(20),}}>
+              <Text style={{fontSize: moderateScale(30), fontWeight: 'bold'}}>{'ConfiaShop no disponible\n'}</Text>
+              <Text style={{justifyContent: 'center'}}>Lo sentimos, por el momento el servicio de ConfiaShop no esta disponible para esta sesión</Text>
+            </View>}
           </SafeAreaView>
         </View>
       </Container >
